7b2054c4d9b2eba479e16a84967e8f410aa92bb3
Jovan <45543803+wanjoh@users.noreply.github.com>
2025-09-26 21:25:48 +0200

    Graph class added

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <vector>
+#include <utility>
+#include <span>
+
+class Graph
+{
+public:
+    using Node = int;
+    using Weight = float;
+    using Neighbor = std::pair<Node, Weight>;
+    explicit Graph(int numNodes);
+
+    void addEdge(Node nodeU, Node nodeV, Weight weight) noexcept;
+    
+    [[nodiscard]] std::span<const Neighbor> getNeighbors(Node n) const noexcept;
+    [[nodiscard]] int getNumNodes() const noexcept { return static_cast<int>(m_adjacencyList.size()); }
+    [[nodiscard]] int degree(Node n) const noexcept;
+
+    void finalize() noexcept { m_sealed = true;}
+private:
+    std::vector<std::vector<Neighbor>> m_adjacencyList;
+    bool m_sealed;
+};
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,1 +1,45 @@
 #include "graph.hpp"
+
+#include <algorithm>
+#include <cassert>
+
+
+Graph::Graph(int numNodes) 
+    : m_adjacencyList(numNodes)
+    , m_sealed(false) 
+{
+}
+
+void Graph::addEdge(Node nodeU, Node nodeV, Weight weight) 
+{
+    assert( nodeU >= 0 && nodeU < getNumNodes() && 
+            nodeV >= 0 && nodeV < getNumNodes() &&
+            "Node indices must be within valid range.");
+    assert(!m_sealed && "Cannot add edges to a sealed graph.");
+
+    auto insert_unique = [](std::vector<Neighbor>& vec, Node to, Weight w) 
+    {
+        if (std::none_of(vec.begin(), vec.end(), [&](const Neighbor& n) {return n.first == to;}))
+        {
+            vec.emplace_back(to, w);
+        }
+    };
+    insert_unique(m_adjacencyList[nodeU], nodeV, weight);
+    insert_unique(m_adjacencyList[nodeV], nodeU, weight);
+}
+
+std::span<const Graph::Neighbor> Graph::getNeighbors(Node node) const 
+{
+    if (node < 0 || node >= static_cast<int>(m_adjacencyList.size()))
+    {
+        return {};
+    }
+
+    const auto& v = m_adjacencyList[node];
+    return {v.data(), v.size()};
+}
+
+int Graph::degree(Node n) const 
+{
+    return static_cast<int>(getNeighbors(n).size());
+}
